a496f9f45fd630475c7fde86c01f68643dab6105
Sri170303 <121078655+Sri170303@users.noreply.github.com>
2023-12-28 17:39:38 +0530

    _10_.c

--- Ass_1_printf and scanf/_10_.c
+++ Ass_1_printf and scanf/_10_.c
@@ -1,4 +1,4 @@
-//  WAP to find the area of a circle. The radius is given as input by the user 
+// WAP to find the area of a circle. The radius is given as input by the user 
 // Output Format - "Area of circle is A having the radius R". Replace A with area and R with radius
 #include <stdio.h>
 int main(){
